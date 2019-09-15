11ab95806596c95b332c63d8af5d8b99fee2a940
sean7723 <schiu8958@bths.edu>
2019-09-15 17:56:58 -0400

    Added Link

--- ub_course_viewer/src/index.js
+++ ub_course_viewer/src/index.js
@@ -1,5 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+< link
+rel = "stylesheet"
+href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+crossorigin = "anonymous" /
+    >
+    import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
