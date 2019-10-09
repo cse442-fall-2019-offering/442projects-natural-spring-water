092f54ee254d99655c3de95ebb5547a3c7f8743d
sean7723 <schiu8958@bths.edu>
2019-10-09 02:28:33 -0400

    Used <Canvas> to make connected cells

--- ub_course_viewer/src/core/Core.js
+++ ub_course_viewer/src/core/Core.js
@@ -1,25 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import Rectangle from 'react-rectangle'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Search from '../landingPage/Search';
+import '../App.css';
 
 function Core(){
     return (
-        <div>
-            <Container>
-                <Row>
-                    <Col xs={1}>Home Page Button</Col>
-                    <Col><Search></Search></Col>
-                </Row>
-                <Row>
-                    <Col>Course List</Col>
-                    <Col>Core Viewer</Col>
-                    <Col>Cart</Col>
-                </Row>
-            </Container>
-        </div>
+		<div>
+			<Search> </Search>
+			
+			<canvas id = "graph" width="200" height="100" style={{border: "1px solid #000000"}}> <span> Hello </span> </canvas>
+			<canvas id = "graph" width="200" height="0" style={{border: "1px solid #000000", position: "relative", bottom: "40pt"}}> </canvas>
+			<canvas id = "graph" width="200" height="100" style={{border: "1px solid #000000"}}> </canvas>
+			
+		</div>
     );
 }
 
