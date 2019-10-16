01fd432e0f05d5912fa6b441a423330517599319
unknown <jromano6556@gmail.com>
2019-10-16 11:24:39 -0400

    Added a basic example of a graph using react-sigma

--- ub_course_viewer/src/core/Core.js
+++ ub_course_viewer/src/core/Core.js
@@ -6,6 +6,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Search from '../landingPage/Search';
 import '../App.css';
+import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
+
+let myGraph = {nodes:[{id:"n1", label:"CSE250"}, {id:"n2", label:"CSE191"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]};
 
 function Core(){
     return (
@@ -15,7 +18,10 @@ function Core(){
 			<canvas id = "graph" width="200" height="100" style={{border: "1px solid #000000"}}> <span> Hello </span> </canvas>
 			<canvas id = "graph" width="200" height="0" style={{border: "1px solid #000000", position: "relative", bottom: "40pt"}}> </canvas>
 			<canvas id = "graph" width="200" height="100" style={{border: "1px solid #000000"}}> </canvas>
-			
+			<Sigma graph={myGraph} settings={{drawEdges: true, clone: false}}>
+				<RelativeSize initialSize={15}/>
+				<RandomizeNodePositions/>
+			</Sigma>
 		</div>
     );
 }
