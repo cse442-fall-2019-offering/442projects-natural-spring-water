b7e1853c56307ae4b9076e01975892a85a0dabd5
Wilson Wong <wwong9@buffalo.edu>
2019-11-11 23:35:46 -0500

    Added Axios to App.js #43

--- ub_course_viewer/src/App.js
+++ ub_course_viewer/src/App.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 import './App.css';
 import LandingPage from "./landingPage/LandingPage";
 import Core from "./core/Core"
-
+import axios from 'axios';
 // Currently the landing page that holds the Search and courseElement components
 
 class App extends Component {
@@ -30,7 +30,7 @@ class App extends Component {
 		let cart = this.state.my_shopping_cart;
 		var index;
 		index = cart.indexOf(courseCode);
-		
+
 		console.log("App.js : remove_course() : index " + index)
 		if(index !== -1) {
 			cart.splice(index , 1);
@@ -47,15 +47,19 @@ class App extends Component {
 		let cart = this.state.my_shopping_cart;
 		this.setState({graph_viewer: cart});
 	}
-	
+
 	callAPI() {
-	    fetch("http://localhost:9000/myAPI")
-	        .then(res => res.text())
-			.then(res => this.setState({ myAPIResponse: JSON.parse(res) }));
-			
+	    // fetch("http://localhost:9000/myAPI")
+	    //     .then(res => res.text())
+		// 	.then(res => this.setState({ myAPIResponse: JSON.parse(res) }));
+	const url = 'test.php';
+	axios.get(url).then(response => response.data).then((data) => {
+		console.log(data);
+	});
+
 	}
 
-	componentWillMount() {
+	componentDidMount() {
 		this.callAPI();
 	}
 
@@ -73,12 +77,12 @@ class App extends Component {
 	render() {
 		return(
 			<div>
-				{ 
+				{
 					this.state.showLandingPage ?
 						<LandingPage onSearch={this.updateSearchString} onAdd={this.add_course} cart={this.getCart} onRemove={this.remove_course} courses={this.getCourseResults()} searchString={this.state.searchString} onOpen={this.openCourse}></LandingPage> : null
 				}
-				{ 
-					this.state.showCore ? 
+				{
+					this.state.showCore ?
 						<Core cart={this.state.my_shopping_cart} selectedCourse={this.state.selectedCourse} courseList={this.getCourseResults()}></Core>: null
 				}
 			</div>
