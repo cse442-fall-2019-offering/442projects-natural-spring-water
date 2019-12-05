ea89e6a07167c18eac66b08ea6c7b7ba48388faa
Sean Chiu <seanchiu@cheshire.cse.buffalo.edu>
2019-12-05 05:30:07 -0500

    Added Modal Popup for Topics Button in each Course Element

--- ub_course_viewer/src/App.js
+++ ub_course_viewer/src/App.js
@@ -7,97 +7,140 @@ import axios from 'axios';
 // Currently the landing page that holds the Search and courseElement components
 
 class App extends Component {
-	state = {
-		my_shopping_cart: [],
-		showLandingPage: true,
-		showCore: true,
-		graph_viewer: [],
-		myAPIResponse: [],
-		searchString: "",
-		selectedCourse:{}
-	}
+        state = {
+                my_shopping_cart: [],
+                showLandingPage: true,
+                showCore: false,
+                graph_viewer: [],
+                courses: [],
+                searchString: "",
+                selectedCourse:{},
+                topics: []
+        }
 
-	add_course = (courseCode) => {
-		//triple equals because console wouldnt shut the fuck up about it
-		let cart = this.state.my_shopping_cart;
-		if(cart.indexOf(courseCode) === -1){
-			cart.push(courseCode);
-		}
-		this.setState({my_shopping_cart: cart});
-	}
+        add_course = (courseCode) => {
+                //triple equals because console wouldnt shut the fuck up about it
+                let cart = this.state.my_shopping_cart;
+                if(cart.indexOf(courseCode) === -1){
+                        cart.push(courseCode);
+                }
+                this.setState({my_shopping_cart: cart});
+        }
 
-	remove_course = (courseCode) => {
-		let cart = this.state.my_shopping_cart;
-		var index;
-		index = cart.indexOf(courseCode);
+        remove_course = (courseCode) => {
+                let cart = this.state.my_shopping_cart;
+                var index;
+                index = cart.indexOf(courseCode);
 
-		console.log("App.js : remove_course() : index " + index)
-		if(index !== -1) {
-			cart.splice(index , 1);
-			console.log("App.js : remove_course() : Removed")
-		}
-		this.setState({my_shopping_cart: cart})
-	}
+                console.log("App.js : remove_course() : index " + index)
+                if(index !== -1) {
+                        cart.splice(index , 1);
+                        console.log("App.js : remove_course() : Removed")
+                }
+                this.setState({my_shopping_cart: cart})
+        }
 
-	getCart = () => {
-		return this.state.my_shopping_cart;
-	}
+        getCart = () => {
+                return this.state.my_shopping_cart;
+        }
 
-	openGraph = () => {
-		let cart = this.state.my_shopping_cart;
-		this.setState({graph_viewer: cart});
-	}
+        openGraph = () => {
+                let cart = this.state.my_shopping_cart;
+                this.setState({graph_viewer: cart});
+        }
 
-	setmyAPIResponse = (response) => {
-		var processed_response = response.replace(/\\/g, "");
-		processed_response = processed_response.replace(/\"{/g, "{");
-		processed_response = processed_response.replace(/}\"/g, "}");
-		this.setState({myAPIResponse: JSON.parse(processed_response)});
-	}
+        processResponse = (response) => {
+                var processed_response = response.replace(/\\/g, "");
+                processed_response = processed_response.replace(/\"{/g, "{");
+                processed_response = processed_response.replace(/}\"/g, "}");
+                console.log(processed_response);
+                return processed_response;
+        }
 
-	callAPI(my_callback) {
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				//console.log("Non-JSON-Parsed: "+xhttp.responseText);
-				//console.log("JSON-Parsed: "+JSON.parse(xhttp.responseText));
-				my_callback(xhttp.responseText);
-			}
-		};
-		xhttp.open("GET", "https://www-student.cse.buffalo.edu/CSE442-542/2019-Fall/cse-442n/build/index_0.php", true);
-		xhttp.send();
-	}
+        courseTopicsToIntArr = course => {
+                if(course.associated_topics==null) return [];
+                course.associated_topics = course.associated_topics.split(",").map(Number);
+                return course;
+        }
 
-	componentDidMount() {
-		this.callAPI(this.setmyAPIResponse);
-	}
+        processTopicsInCourse = (courseList) => {
+                var temp_courses = courseList;
+                temp_courses = temp_courses.map(this.courseTopicsToIntArr);
+                return temp_courses;
+        }
 
-	getCourseResults = () => {
-		return this.state.myAPIResponse;
-	}
+        setCourses = (response) => {
+                var processed_response = this.processResponse(response);
+                this.setState({courses: this.processTopicsInCourse(JSON.parse(processed_response))});
+//              this.setState({courses: this.processTopicsInCourse(this.state.courses)});
+        }
 
-	updateSearchString = (newSearch) => {
-		this.setState({searchString: newSearch});
-	}
+        setTopics = (response) => {
+                var processed_response = this.processResponse(response);
+                this.setState({topics: JSON.parse(processed_response)});
+                console.log(this.state.topics[0]);
+        }
 
-	openCourse = (course) =>{
-		this.setState({selectedCourse:course});
-	}
-	render() {
-		return(
-			<div>
-				{
-					this.state.showLandingPage ?
-						<LandingPage onSearch={this.updateSearchString} onAdd={this.add_course} cart={this.getCart} onRemove={this.remove_course} courses={this.getCourseResults()} searchString={this.state.searchString} onOpen={this.openCourse}></LandingPage> : null
-				}
-				{
-					this.state.showCore ?
-						<Core cart={this.state.my_shopping_cart} selectedCourse={this.state.selectedCourse} courseList={this.getCourseResults()}></Core>: null
-				}
-			</div>
-		);
-	}
+        callAPI(my_callback, phpFile) {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                                //console.log("Non-JSON-Parsed: "+xhttp.responseText);
+                                //console.log("JSON-Parsed: "+JSON.parse(xhttp.responseText));
+                                my_callback(xhttp.responseText);
+                        }
+                };
+                xhttp.open("GET", "https://www-student.cse.buffalo.edu/CSE442-542/2019-Fall/cse-442n/build3/"+phpFile , true);
+                xhttp.send();
+        }
+
+        componentDidMount = () => {
+                this.callAPI(this.setCourses,"index_0.php");
+                this.callAPI(this.setTopics,"topics.php");
+                console.log(this.state.courses);
+                console.log(this.state.topics);
+        }
+
+        getCourseResults = () => {
+                return this.state.courses;
+        }
+
+        getTopics = () => {
+                return this.state.topics;
+        }
+
+        updateSearchString = (newSearch) => {
+                this.setState({searchString: newSearch});
+        }
+
+        openCourse = (course) =>{
+                this.setState({selectedCourse:course});
+        }
+
+        togglePage = () =>{
+                console.log("Something happens")
+                this.setState({showLandingPage:(!this.state.showLandingPage), showCore:(!this.state.showCore)});
+        }
+
+        render() {
+                return(
+                        <div>
+                                <button onClick={this.togglePage}>Page Swap</button>
+                                {
+                                        this.state.showLandingPage ?
+                                                <LandingPage onSearch={this.updateSearchString} onAdd={this.add_course} cart={this.getCart} onRemove={this.remove_course} courses={this.getCourseResults()} topics={this.getTopics()} searchString={this.state.searchString} onOpen={this.openCourse}></LandingPage> : null
+
+                                }
+
+                                {
+                                        this.state.showCore ?
+                                                <Core cart={this.state.my_shopping_cart} selectedCourse={this.state.selectedCourse} courseList={this.getCourseResults()} topicList={this.getTopics()}></Core>: null
+
+                                }
+                        </div>
+                );
+        }
 }
 
-
 export default App;
+
