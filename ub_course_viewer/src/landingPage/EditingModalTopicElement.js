41d48f248241d15ef3bf9d0495c0dfba6278a69c
Wilson Wong <wwong9@cheshire.cse.buffalo.edu>
2019-12-05 21:30:16 -0500

    Added a vertical scrollbar to the Course Editing Overlay, #78

--- ub_course_viewer/src/landingPage/EditingModalTopicElement.js
+++ ub_course_viewer/src/landingPage/EditingModalTopicElement.js
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{Component} from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class EditingModalTopicElement extends Component{
 
@@ -17,11 +19,10 @@ class EditingModalTopicElement extends Component{
 		return(
 			<>
 				<ListGroup.Item>
-                    {this.props.topic_desc}
 					{
 						this.state.is_associated ?
-						<Button onClick={()=>{this.setState({is_associated: false});this.props.remove_from_cart(this.props.topic_id)}} variant="secondary">Remove</Button>
-						: <Button onClick={()=>{this.setState({is_associated: true});this.props.add_to_cart(this.props.topic_id)}} variant="success">Add</Button>
+						<Row noGutters = "true"> <Col xs = {9}> <li class="list-group-item"> {this.props.topic_desc} </li> </Col> <Col> <Button style={{width:"100%",height:"100%"}} onClick={()=>{this.setState({is_associated: false});this.props.remove_from_cart(this.props.topic_id)}} variant="secondary">Remove</Button> </Col> </Row>
+						: <Row noGutters = "true"> <Col xs = {10}> <li class="list-group-item"> {this.props.topic_desc} </li> </Col> <Col> <Button style={{width:"100%",height:"100%"}} onClick={()=>{this.setState({is_associated: true});this.props.add_to_cart(this.props.topic_id)}} variant="success">Add</Button> </Col> </Row>
 					}
 				</ListGroup.Item>
 			</>
